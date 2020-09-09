0266c351b1b9b124e58c1aa95be2acf45e5fb708
Никита Порубаймех <np.basis@MacBook-Pro-Nikita.local>
2020-09-09 12:47:19 +0300

    Updates

---
+++ precache-manifest.aa7b02d50413ef75cc86781db532b9e5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f7a3e7c2bc192f827381225b883e22",
+    "url": "/vezdekod-hf-web/index.html"
+  },
+  {
+    "revision": "422dd48bc91e9e036e97",
+    "url": "/vezdekod-hf-web/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "3653dceec2bb1395e4ce",
+    "url": "/vezdekod-hf-web/static/js/2.84911def.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/vezdekod-hf-web/static/js/2.84911def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422dd48bc91e9e036e97",
+    "url": "/vezdekod-hf-web/static/js/main.cae094e0.chunk.js"
+  },
+  {
+    "revision": "a11e8486537b6a66976c",
+    "url": "/vezdekod-hf-web/static/js/runtime-main.148c24ca.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/vezdekod-hf-web/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/vezdekod-hf-web/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/vezdekod-hf-web/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/vezdekod-hf-web/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/vezdekod-hf-web/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/vezdekod-hf-web/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/vezdekod-hf-web/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/vezdekod-hf-web/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/vezdekod-hf-web/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/vezdekod-hf-web/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/vezdekod-hf-web/static/media/sub.4eb05fdd.png"
+  }
+]);
